2cde2c2e1d741883739f51ba8cf4b410bf6f2792
Mateus Henrique <mateushcandido5@gmail.com>
2023-05-01 12:43:03 -0300

    refactoring

--- hr-oauth/src/main/java/com/mtzz/domain/Account.java
+++ hr-oauth/src/main/java/com/mtzz/domain/Account.java
@@ -4,11 +4,13 @@ import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.core.userdetails.UserDetails;
 
 import java.util.Collection;
 import java.util.HashSet;
 import java.util.Set;
+import java.util.stream.Collectors;
 
 @Data
 @NoArgsConstructor
@@ -18,48 +20,50 @@ public class Account implements UserDetails
     private Long accountId;
     private String nameAccount;
     private String emailAccount;
+    private String passwordAccount;
     private Set<AccessPermission> permissions = new HashSet<>();
 
 
     @Override
     public Collection<? extends GrantedAuthority> getAuthorities()
     {
-        return null;
-    }
-
-    @Override
-    public String getPassword()
-    {
-        return null;
+        return permissions.stream().map(x -> new SimpleGrantedAuthority(x.getPermissionType()))
+                .collect(Collectors.toList());
     }
 
     @Override
     public String getUsername()
     {
-        return null;
+        return emailAccount;
+    }
+
+    @Override
+    public String getPassword()
+    {
+        return passwordAccount;
     }
 
     @Override
     public boolean isAccountNonExpired()
     {
-        return false;
+        return true;
     }
 
     @Override
     public boolean isAccountNonLocked()
     {
-        return false;
+        return true;
     }
 
     @Override
     public boolean isCredentialsNonExpired()
     {
-        return false;
+        return true;
     }
 
     @Override
     public boolean isEnabled()
     {
-        return false;
+        return true;
     }
 }
